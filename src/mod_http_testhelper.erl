70bc0c9e2c8b7a8e3e6d1426badb425d4bb791f1
Luca Greco <luca.greco@alcacoop.it>
2010-02-20 16:21:43 +0100

    fix license

--- src/mod_http_testhelper.erl
+++ src/mod_http_testhelper.erl
@@ -1,11 +1,30 @@
 %%%----------------------------------------------------------------------
 %%% File    : mod_http_testhelper.erl
-%%% Author  : Luca Greco <luca.greco [at] alcacoop [dot] it>
+%%% Author  : Luca Greco <luca.greco@alcacoop.it>
 %%% Purpose : simple ejabberd http request handler to serve helper useful
 %%%           during testing of xmpp code
-%%% License : LGPLv3
-%%% Created : 2010-02-19
+%%% License : GPLv2
+%%% Created : 19 Feb 2010 by Luca Greco <luca.greco@alcacoop.it>
 %%% Id      :
+%%%----------------------------------------------------------------------
+%%%
+%%% mod_http_testhelper.erl, Copyright (C) 2010 Alca Società Cooperativa
+%%%
+%%% This program is free software; you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation; either version 2 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%%% General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License
+%%% along with this program; if not, write to the Free Software
+%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+%%% 02111-1307 USA
+%%%
 %%%----------------------------------------------------------------------
 
 -module(mod_http_testhelper).
